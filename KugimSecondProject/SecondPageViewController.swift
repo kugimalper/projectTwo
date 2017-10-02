d9e95aef9a16868d7af2932b29d86e78a085e928
ST5 <st5@ST5-iMac.local>
2017-10-02 14:28:50 +0300

    when the name entered to the textField there will be 2 options 1, click the send button 2, in keyboard press go

--- KugimSecondProject/SecondPageViewController.swift
+++ KugimSecondProject/SecondPageViewController.swift
@@ -8,28 +8,48 @@
 
 import UIKit
 
-class SecondPageViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class SecondPageViewController: UIViewController, UITextFieldDelegate { // IUTextFieldDelegate
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sendButtonOutlet: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBAction func sendButton(_ sender: UIButton) {
+       nameLabel.text =  textField.text!
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       sendButton(sendButtonOutlet)
+        return true
     }
-    */
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+    
+    
 }
